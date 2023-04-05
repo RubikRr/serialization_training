11ac4df4f2a6d651ed79c2205057e551a10c7774
Ukropchik <vova.chakalov.2003@gmail.com>
2023-04-05 20:09:28 +0300

    Add project files.

---
+++ ConsoleApp1/Shoes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    [Serializable]
+    internal class Shoes
+    {
+        public string VendorCode;
+        public string Name;
+        public int Count;
+        public int Cost;
+        public Shoes(string vendorCode, string name, int count, int cost)
+        {
+            VendorCode = vendorCode;
+            Name = name;
+            Count = count;
+            Cost = cost;
+        }
+
+        public override string ToString()
+        {
+            return $"{VendorCode}    {Name}    {Count}    {Cost}";
+        }
+    }
+}
